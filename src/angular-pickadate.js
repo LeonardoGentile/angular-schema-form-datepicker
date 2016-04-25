390fbb78cd27b6bb96dd3f1f46237303e20e11de
Leonardo Gentile <leonard.gentile@gmail.com>
2016-04-25 12:39:04 +0200

    Properly passing options to picker
    From https://github.com/leandropio/angular-schema-form-datepicker

--- src/angular-pickadate.js
+++ src/angular-pickadate.js
@@ -22,57 +22,79 @@ angular.module('schemaForm').directive('pickADate', function () {
       format: '='
     },
     link: function (scope, element, attrs, ngModel) {
-      //Bail out gracefully if pickadate is not loaded.
-      if (!element.pickadate) {
-        return;
-      }
-
+      var picker;
+      var timeoutDeduplicate;
+      var pickedElem;
+      var runOnceUndone = true;
       //By setting formatSubmit to null we inhibit the
       //hidden field that pickadate likes to create.
       //We use ngModel formatters instead to format the value.
-      var opts = {
+      var basicOptions = {
         onClose: function () {
           element.blur();
         },
         formatSubmit: null
       };
-      if (scope.pickADate) {
-        angular.extend(opts, scope.pickADate);
-      }
-      element.pickadate(opts);
 
-      //Defaultformat is for json schema date-time is ISO8601
-      //i.e.  "yyyy-mm-dd"
-      var defaultFormat = 'yyyy-mm-dd';
-
-      //View format on the other hand we get from the pickadate translation file
-      var viewFormat    = $.fn.pickadate.defaults.format;
-
-      var picker = element.pickadate('picker');
-
-      //The view value
-      ngModel.$formatters.push(function(value) {
-        if (angular.isUndefined(value) || value === null) {
-          return value;
+      var exec = function( externalOptions ){
+        //Bail out gracefully if pickadate is not loaded.
+        if (!element.pickadate) {
+          return;
         }
 
-        //We set 'view' and 'highlight' instead of 'select'
-        //since the latter also changes the input, which we do not want.
-        picker.set('view', value, {format: scope.format || defaultFormat});
-        picker.set('highlight', value, {format: scope.format || defaultFormat});
+        if( !externalOptions || !externalOptions.constructor.name === "Object" ){
 
-        //piggy back on highlight to and let pickadate do the transformation.
-        return picker.get('highlight', viewFormat);
-      });
+          if (angular.isDefined(attrs.pickADateOptions) && attrs.pickADateOptions.constructor.name === "Object") {
+            externalOptions = attrs.pickADateOptions;
+          }else {
+            externalOptions = {};
+          };
+        }
 
-      ngModel.$parsers.push(function() {
-        return picker.get('select', scope.format || defaultFormat);
-      });
+        var fullOptions = angular.extend({}, basicOptions, externalOptions );
+
+        pickedElem = element.pickadate( fullOptions );
+
+        //Defaultformat is for json schema date-time is ISO8601
+        //i.e.  "yyyy-mm-dd"
+        var defaultFormat = 'yyyy-mm-dd';
+
+        //View format on the other hand we get from the pickadate translation file
+        var viewFormat    = $.fn.pickadate.defaults.format;
+
+        picker = element.pickadate('picker');
+
+        // Some things have to run only once or they freeze the browser!
+        if( runOnceUndone ){
+
+          //The view value
+          ngModel.$formatters.push(function(value) {
+            if (angular.isUndefined(value) || value === null) {
+              return value;
+            }
+
+            //We set 'view' and 'highlight' instead of 'select'
+            //since the latter also changes the input, which we do not want.
+            picker.set('view', value, {format: scope.format || defaultFormat});
+            picker.set('highlight', value, {format: scope.format || defaultFormat});
+
+            //piggy back on highlight to and let pickadate do the transformation.
+            return picker.get('highlight', viewFormat);
+          });
+
+          ngModel.$parsers.push(function() {
+            return picker.get('select', scope.format || defaultFormat);
+          });
+
+          runOnceUndone = false;
+        };
+
+      }; // /exec
 
       //bind once.
       if (angular.isDefined(attrs.minDate)) {
         var onceMin = scope.$watch('minDate', function (value) {
-          if (value) {
+          if ( value && picker ) {
             picker.set('min', formatDate(value));
             onceMin();
           }
@@ -81,12 +103,29 @@ angular.module('schemaForm').directive('pickADate', function () {
 
       if (angular.isDefined(attrs.maxDate)) {
         var onceMax = scope.$watch('maxDate', function (value) {
-          if (value) {
+          if (value && picker) {
             picker.set('max', formatDate(value));
             onceMax();
           }
         }, true);
       }
-    }
+
+      if (angular.isDefined(attrs.pickADate)) {
+        var onceOptions = scope.$watch('pickADate', function (value) {
+
+          if( value && picker && value.constructor.name === "Object" ){
+
+            picker.stop();
+            clearTimeout( timeoutDeduplicate );
+            timeoutDeduplicate = setTimeout(function() {
+              exec( value );
+            }, 1500);
+            onceOptions();
+          };
+        }, true);
+      };
+
+      exec();
+    } // /link
   };
 });
